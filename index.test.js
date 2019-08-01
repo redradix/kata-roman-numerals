672cb7bdf3095e631ea5b00843ca39ff401b7a83
Victor Miguez <victor.miguez.martinez@gmail.com>
2019-08-01 12:20:00 +0200

    :art: clean tests

--- index.test.js
+++ index.test.js
@@ -4,72 +4,27 @@ const {
 
 describe('Kata Roman Numerals', () => {
   const SAMPLES = [
-    {
-      title: '1 returns I',
-      input: 1,
-      expected: 'I'
-    },
-    {
-      title: '2 returns II',
-      input: 2,
-      expected: 'II'
-    },
-    {
-      title: '3 returns III',
-      input: 3,
-      expected: 'III'
-    },
-    {
-      title: '4 returns IV',
-      input: 4,
-      expected: 'IV'
-    },
-    {
-      title: '5 returns V',
-      input: 5,
-      expected: 'V'
-    },
-    {
-      title: '6 returns VI',
-      input: 6,
-      expected: 'VI'
-    },
-    {
-      title: '9 returns IX',
-      input: 9,
-      expected: 'IX'
-    },
-    {
-      title: '10 returns X',
-      input: 10,
-      expected: 'X'
-    },
-    {
-      title: '14 returns XIV',
-      input: 14,
-      expected: 'XIV'
-    },
-    {
-      title: '40 returns XL',
-      input: 40,
-      expected: 'XL'
-    },
-    {
-      title: '41 returns XLI',
-      input: 41,
-      expected: 'XLI'
-    }
+    { input: 1, expected: 'I' },
+    { input: 2, expected: 'II' },
+    { input: 3, expected: 'III' },
+    { input: 4, expected: 'IV' },
+    { input: 5, expected: 'V' },
+    { input: 6, expected: 'VI' },
+    { input: 9, expected: 'IX' },
+    { input: 10, expected: 'X' },
+    { input: 14, expected: 'XIV' },
+    { input: 40, expected: 'XL' },
+    { input: 41, expected: 'XLI' }
   ]
 
   SAMPLES.forEach(sample => {
 
     const {
-      title,
       input,
       expected
     } = sample
 
-    it(title, () => {
+    it(`${input} should be ${expected}`, () => {
       expect(toRoman(input)).toBe(expected)
     })
   })
