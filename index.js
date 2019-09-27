58353b439c030d550cd0f745064285d2c6077a89
Andres Martin <monkeyandres@protonmail.com>
2019-09-27 15:53:02 +0200

    Green for converting 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const numberToRoman = number => {
   if (number === 5) return 'V'
-  return number === 1 ? 'I' : 'II'
+  return number === 1 ? 'I' : number === 2 ? 'II' : 'III'
 }
 
 module.exports = {
