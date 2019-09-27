aed6a15026ffa3097b87652eebbbbfaf91e4d5e2
Andres Martin <monkeyandres@protonmail.com>
2019-09-27 16:54:08 +0200

    Refactor converting 15

--- index.js
+++ index.js
@@ -15,13 +15,10 @@ const numberToRoman = number => {
       return 'V' + numberToRoman(number - 5)
     case 9:
       return 'IX'
-    case 10:
-    case 11:
-    case 12:
-    case 13:
-    case 14:
-    case 15:
-      return 'X' + numberToRoman(number - 10)
+  }
+
+  if (number >= 10) {
+    return 'X' + numberToRoman(number - 10)
   }
 }
 
