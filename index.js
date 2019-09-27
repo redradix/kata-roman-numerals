cd54cbe9c4cb5377d1d6f26ed2b9fbedce2ee0f9
Andres Martin <monkeyandres@protonmail.com>
2019-09-27 17:31:14 +0200

    Refactor converting 100

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const VALUE_SYMBOLS = [
   [100, 'C'],
+  [90, 'XC'],
   [50, 'L'],
   [40, 'XL'],
   [10, 'X'],
+  [9, 'IX'],
   [5, 'V'],
-  [1, 'I']
+  [4, 'IV'],
+  [1, 'I'],
+  [0, '']
 ]
 
 const numberToRoman = number => {
-  switch (number) {
-    case 0:
-      return ''
-    case 4:
-      return 'IV'
-    case 9:
-      return 'IX'
-    case 40:
-      return 'XL'
-    case 90:
-      return 'XC'
-  }
+  if (number === 0) return ''
 
   for (const [value, symbol] of VALUE_SYMBOLS) {
     if (number >= value) {
