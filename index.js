2695595a9d85e5fb9282457edbee2834ce86c777
Alejandro Rios <alejandro@redradix.com>
2019-09-27 16:12:48 +0200

    case 8 green

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 function romanNumbers(number) {
-  switch (number){
-    default:
-        let r = ''
-        for (let i=0; i<number; i++) {
-          r += 'I'
-        }
-        return r
-    case 4:
-      return 'IV'
-    case 5:
-      return 'V'
-    case 6:
-      return 'VI'
-    case 7: 
-      return 'VII'
+  if (number <= 3){
+    let r = ''
+    for (let i=0; i<number; i++) {
+      r += 'I'
+    }
+    return r
+  }
+  else if (number === 4) return 'IV';
+  else if (number >= 5){
+    let r = 'V'
+    let rest = number - 5
+
+    for (let i = rest; rest > 0; rest--) {
+      r += 'I'
+    } 
+    return r
   }
 }
 
