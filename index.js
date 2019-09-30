70d0b28f2880c4dcdec116e91f12039e7e337866
Kevin Perree <kevin@redradix.com>
2019-09-30 10:19:37 +0200

    green: extending arabic and roman table

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = {
 
 function arabicToRoman(numberToConvert) {
   let romanResult = '';
-  const ARABIC_TABLE = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  const ROMAN_TABLE = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  const ARABIC_TABLE = [5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const ROMAN_TABLE = ["V", "IV", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
   for (let i = 0; i <= ARABIC_TABLE.length; i++) {
     while (numberToConvert % ARABIC_TABLE[i] < numberToConvert) {
       romanResult += ROMAN_TABLE[i];
