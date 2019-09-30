a1050fe7524f4cb4633c014c94b74957093a1046
Kevin Perree <kevin@redradix.com>
2019-09-30 11:40:25 +0200

    red: bidirectional tests

--- index.test.js
+++ index.test.js
@@ -1,50 +1,55 @@
 const {
-  arabicToRoman
+  arabicToRoman,
+  fromRoman
 } = require('./index')
 
 describe('Kata Roman Numerals', () => {
   const SAMPLES = [
-    { input: 1, expected: 'I' },
-    { input: 2, expected: 'II' },
-    { input: 3, expected: 'III' },
-    { input: 4, expected: 'IV' },
-    { input: 5, expected: 'V' },
-    { input: 6, expected: 'VI' },
-    { input: 9, expected: 'IX' },
-    { input: 10, expected: 'X' },
-    { input: 14, expected: 'XIV' },
-    { input: 40, expected: 'XL' },
-    { input: 41, expected: 'XLI' },
-    { input: 49, expected: 'XLIX' },
-    { input: 50, expected: 'L' },
-    { input: 51, expected: 'LI' },
-    { input: 90, expected: 'XC' },
-    { input: 91, expected: 'XCI' },
-    { input: 99, expected: 'XCIX' },
-    { input: 100, expected: 'C' },
-    { input: 400, expected: 'CD' },
-    { input: 401, expected: 'CDI' },
-    { input: 500, expected: 'D' },
-    { input: 900, expected: 'CM' },
-    { input: 901, expected: 'CMI' },
-    { input: 1000, expected: 'M' },
-    { input: 1999, expected: 'MCMXCIX' },
-    { input: 3000, expected: 'MMM' },
-    { input: 3334, expected: 'MMMCCCXXXIV' },
-    { input: 4000, expected: 'IV' },
-    { input: 4444, expected: 'IVCDXLIV' },
-    { input: 5000, expected: 'V' }
+    { arabic: 1, roman: 'I' },
+    { arabic: 2, roman: 'II' },
+    { arabic: 3, roman: 'III' },
+    { arabic: 4, roman: 'IV' },
+    { arabic: 5, roman: 'V' },
+    { arabic: 6, roman: 'VI' },
+    { arabic: 9, roman: 'IX' },
+    { arabic: 10, roman: 'X' },
+    { arabic: 14, roman: 'XIV' },
+    { arabic: 40, roman: 'XL' },
+    { arabic: 41, roman: 'XLI' },
+    { arabic: 49, roman: 'XLIX' },
+    { arabic: 50, roman: 'L' },
+    { arabic: 51, roman: 'LI' },
+    { arabic: 90, roman: 'XC' },
+    { arabic: 91, roman: 'XCI' },
+    { arabic: 99, roman: 'XCIX' },
+    { arabic: 100, roman: 'C' },
+    { arabic: 400, roman: 'CD' },
+    { arabic: 401, roman: 'CDI' },
+    { arabic: 500, roman: 'D' },
+    { arabic: 900, roman: 'CM' },
+    { arabic: 901, roman: 'CMI' },
+    { arabic: 1000, roman: 'M' },
+    { arabic: 1999, roman: 'MCMXCIX' },
+    { arabic: 3000, roman: 'MMM' },
+    { arabic: 3334, roman: 'MMMCCCXXXIV' },
+    { arabic: 4000, roman: 'I_V' },
+    { arabic: 4444, roman: 'I_VCDXLIV' },
+    { arabic: 5000, roman: '_V' }
   ]
 
   SAMPLES.forEach(sample => {
 
     const {
-      input,
-      expected
+      arabic,
+      roman
     } = sample
 
-    it(`Arabic number ${input} should be ${expected} in Roman`, () => {
-      expect(arabicToRoman(input)).toBe(expected)
+    it(`Arabic number ${arabic} should be ${roman} in Roman`, () => {
+      expect(arabicToRoman(arabic)).toBe(roman)
+    })
+
+    it(`Roman number ${roman} should be ${arabic} in Arabic`, () => {
+      expect(fromRoman(roman)).toBe(arabic)
     })
   })
 })
