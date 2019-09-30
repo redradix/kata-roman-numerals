c792c84d55e4ec5e3ae47672dd2a3f61e62f2a24
Kevin Perree <kevin@redradix.com>
2019-09-30 11:41:08 +0200

    green: fonction fromRoman

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 module.exports = {
-  arabicToRoman
+  arabicToRoman,
+  fromRoman
 }
 
 function arabicToRoman(numberToConvert) {
   let romanResult = '';
   const ARABIC_TABLE = [5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  const ROMAN_TABLE = ["V", "IV", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  const ROMAN_TABLE = ["_V", "I_V", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
   for (let i = 0; i <= ARABIC_TABLE.length; i++) {
     while (numberToConvert % ARABIC_TABLE[i] < numberToConvert) {
       romanResult += ROMAN_TABLE[i];
@@ -13,4 +14,17 @@ function arabicToRoman(numberToConvert) {
     }
   }
   return romanResult;
+}
+
+function fromRoman(stringToConvert) {
+  let arabicResult = 0;
+  const ARABIC_TABLE = [5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const ROMAN_TABLE = ["_V", "I_V", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  for (var i = 0;i<=ARABIC_TABLE.length;i++) {
+    while (stringToConvert.indexOf(ROMAN_TABLE[i]) === 0){
+      arabicResult += ARABIC_TABLE[i];
+      stringToConvert = stringToConvert.replace(ROMAN_TABLE[i],'');
+    }
+  }
+  return arabicResult;
 }
